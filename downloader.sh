5f789c52d974fb92117fa734e48bf40a8dc5e5e5
repomansez <sbctani@protonmail.com>
2024-05-29 05:13:54 -0300

    add interactive mode

--- downloader.sh
+++ downloader.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
-test -z $1 || test -z $2 && echo -e "usage: downloader.sh directory link\ne.g downloader.sh hentai https://4chan.org/d/6969" && exit
+interactive(){
 
-mkdir -p $1
-python scrape.py $2 > "$1/links"
-cd $1
+echo -e "directory name: "
+read "dir"
+echo -e "thread link: "
+read "link"
+
+mkdir "$dir" 2>/dev/null  || echo "directory exists, exiting"; exit
+
+python scrape.py "$link" > "$dir/links"
+cd "$dir"
 wget -nc -i links
 
 echo done!
+}
+
+direct(){
+
+mkdir "$1" 2>/dev/null || echo "directory exists, exiting"; exit
+python scrape.py "$2" > "$1/links"
+cd "$1"
+wget -nc -i links
+
+echo done!
+}
+
+
+if [ -z $1 ]; then
+    interactive
+else
+    test -z $1 || test -z $2 && echo -e "usage: downloader.sh directory link\ne.g downloader.sh hentai https://4chan.org/d/6969\nor add no arguments for interactive mode" && exit
+    direct "$1" "$2"
+fi
