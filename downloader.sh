bba3d802bc58e40de8fb40932bb2ddefb9cda05a
repomansez <sbctani@protonmail.com>
2024-05-31 00:38:15 -0300

    slightly better indentation and python invocation

--- downloader.sh
+++ downloader.sh
@@ -1,34 +1,34 @@
-#!/bin/sh
+#!/bin/ksh
 interactive(){
 
-echo -e "directory name: "
-read "dir"
-echo -e "thread link: "
-read "link"
+  echo -e "directory name: "
+  read "dir"
+  echo -e "thread link: "
+  read "link"
 
-mkdir "$dir" 2>/dev/null  || echo "directory exists, exiting"; exit
+  mkdir "$dir" 2>/dev/null  || echo "directory exists, exiting"; exit
 
-python scrape.py "$link" > "$dir/links"
-cd "$dir"
-wget -nc -i links
+  /usr/bin/env python scrape.py "$link" > "$dir/links"
+  cd "$dir"
+  wget -nc -i links
 
-echo done!
+  echo done!
 }
 
 direct(){
 
-mkdir "$1" 2>/dev/null || echo "directory exists, exiting"; exit
-python scrape.py "$2" > "$1/links"
-cd "$1"
-wget -nc -i links
+  mkdir "$1" 2>/dev/null #|| echo "directory exists, exiting"; exit
+  /usr/bin/env python scrape.py "$2" > "$1/links"
+  cd "$1"
+  wget -nc -i links
 
-echo done!
+  echo done!
 }
 
 
 if [ -z $1 ]; then
-    interactive
+  interactive
 else
-    test -z $1 || test -z $2 && echo -e "usage: downloader.sh directory link\ne.g downloader.sh hentai https://4chan.org/d/6969\nor add no arguments for interactive mode" && exit
-    direct "$1" "$2"
+  test -z $1 || test -z $2 && echo -e "usage: downloader.sh directory link\ne.g downloader.sh hentai https://4chan.org/d/6969\nor add no arguments for interactive mode" && exit
+  direct "$1" "$2"
 fi
