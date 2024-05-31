d3b8a382cce9d9951ffdcdc9d4267b06f11b72be
Johnny Doe <sbctani@protonmail.com>
2024-05-31 00:44:06 -0300

    better document what scrape.py is doing

--- scrape.py
+++ scrape.py
@@ -4,10 +4,13 @@ import re
 import string
 from bs4 import BeautifulSoup
 
+# This gets the thread link from downloader.sh and requests its content
 html = requests.get(sys.argv[1]).content
 
 soup = BeautifulSoup(html, 'html.parser')
 
+# This loop finds every link with "is2" in it (4chan cdn) and prints it to stdout
+# which gets outputted into a file by downloader.sh
 for ass in soup.find_all("a", target="_blank"):
     if re.search("is2", str(ass)):
         ass1 = "https:" + ass.get('href')
